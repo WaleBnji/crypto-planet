d2bf117f1d50ee8cacdb9810d552a77e57218dc6
Wale Bnji <walebnji@gmail.com>
2023-11-18 17:59:38 +0100

    Latest commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,37 @@
 // import React from 'react'
-import {AiOutlineSearch} from 'react-icons/ai'
-import {AiOutlineMenuUnfold} from 'react-icons/ai'
+import { useState } from 'react';
+import { AiOutlineSearch } from 'react-icons/ai';
+import { AiOutlineMenuUnfold } from 'react-icons/ai';
+import { AiOutlineClose } from 'react-icons/ai';
 
 const Navbar = () => {
+  const [showNav, setShowNav] = useState(false);
   return (
     <div>
-       <nav className='fbc bg-card h-[40px] wrapper'>
+      <nav className='fbc bg-card h-[80px] wrapper'>
         <h1>Logo</h1>
-        <div className='fbc'>
-        <AiOutlineSearch  className='mr-3'/>
-        <AiOutlineMenuUnfold />
+        <div className={`fbc ${showNav ? 'hidden' : ''} `}>
+          <AiOutlineSearch className='mr-3' />
+          <AiOutlineMenuUnfold
+            onClick={() => {
+              setShowNav(true);
+            }}
+          />
         </div>
+        <AiOutlineClose
+          className={`${!showNav ? 'hidden' : ''}`}
+          onClick={() => {
+            setShowNav(!showNav);
+          }}
+        />
+        {showNav ?? (
+          <div className='h-[100vh] bg-black z-20'>
+            <h1>Navbar</h1>
+          </div>
+        )}
       </nav>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
